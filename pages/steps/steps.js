4c3195cbe674ee8b67dafe86aa65bb4175b9a62b
Nicholas Hallahan <nick@theoutpost.io>
2016-02-20 16:21:27 -0800

    keep track of terminals, fix new terminal button

--- pages/steps/steps.js
+++ pages/steps/steps.js
@@ -1,9 +1,11 @@
-var Steps = {};
+var Steps = {
+	terminals: {}
+};
 
 Steps.openTerminals = function () {
-	tty.openTerminal($('#termDiv1').position().top);
-	tty.openTerminal($('#termDiv2').position().top);
-	tty.openTerminal($('#termDiv3').position().top);
+	Steps.terminals.term1 = tty.openTerminal($('#termDiv1').position().top);
+	Steps.terminals.term2 = tty.openTerminal($('#termDiv2').position().top);
+	Steps.terminals.term3 = tty.openTerminal($('#termDiv3').position().top);
 }
 
 $(function() {
